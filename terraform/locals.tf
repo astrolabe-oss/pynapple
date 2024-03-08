832e9214467db94df5807be3c0ec5f263e3193ab
Patrick Kennel <patrick@pksoftwareco.com>
2024-03-08 15:41:31 -0500

    infra: remote ssh/eks whitelist

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,5 +9,6 @@ locals {
     App         = local.app_name
   }
   ip_address_pk     = "73.70.155.200/32"
-  ip_addresses_devs = [local.ip_address_pk]
+  ip_address_world  = "0.0.0.0/0"
+  ip_addresses_devs = [local.ip_address_pk, local.ip_address_world]
 }
