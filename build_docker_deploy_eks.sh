8b4c17f966d30e166478d154830167a696b27ea6
Patrick Kennel <patrick@pksoftwareco.com>
2024-11-22 12:59:07 -0800

    infra: pynabble/build - simplify and make build scripts configurable

--- build_docker_deploy_eks.sh
+++ build_docker_deploy_eks.sh
@@ -1,46 +1,15 @@
 #!/bin/bash -ex
-RESTART_DEPLOYMENTS=false
-
-# Parse command-line options
-usage() {
-    echo "Usage: $0 [-r] [-h]"
-    echo "  -r,    Conditionally restart the k8s deployments."
-    echo "  -h,    Display this help message and exit."
-}
-
-while getopts ":rh" opt; do
-  case ${opt} in
-    r )
-      RESTART_DEPLOYMENTS=true
-      ;;
-    h )
-      usage
-      exit
-      ;;
-    \? )
-      usage
-      exit 1
-      ;;
-  esac
-done
 
 # APP CONFIG
 APP_NAME1="pynapple1"
 APP_NAME2="pynapple2"
-REPOSITORY1=sandbox1-pynapple1
-REPOSITORY2=sandbox1-pynapple2
+REPOSITORY1="sandbox1-pynapple1"
+REPOSITORY2="sandbox1-pynapple2"
 
 # SANDBOX ENV CONFIG
-AWS_ACCOUNT=112795234683
+AWS_ACCOUNT=$(aws sts get-caller-identity --query "Account" --output text)
 AWS_REGION="us-east-1"
 TIMESTAMP=$(date +%Y%m%d_%H_%M_%S)
-
-# ENSURE AWS ACCOUNT
-CURRENT_AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-if [ "${CURRENT_AWS_ACCOUNT_ID}" != "${AWS_ACCOUNT}" ]; then
-    echo "Error: please be logged in to the CR AWS Account:${EXPECTED_AWS_ACCOUNT_ID}... currently: ${CURRENT_AWS_ACCOUNT_ID}."
-    exit 1
-fi
 
 # LOGIN DOCKER/ECR
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
@@ -60,21 +29,3 @@ docker tag ${APP_NAME1}:${TIMESTAMP} ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazon
 docker push ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY1}:latest
 docker tag ${APP_NAME2}:${TIMESTAMP} ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY2}:latest
 docker push ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY2}:latest
-
-if [ "$RESTART_DEPLOYMENTS" = false ] ; then
-  echo "Not restarting deployments..."
-  exit 0
-fi
-
-echo "Restarting deployments"
-# ENSURE KUBECTL CLUSTER
-KUBERCTL_CONTEXT="arn:aws:eks:${AWS_REGION}:112795234683:cluster/sandbox1-eks"
-CURRENT_CONTEXT=$(kubectl config current-context)
-if [ "${CURRENT_CONTEXT}" != "${KUBERCTL_CONTEXT}" ]; then
-    echo "Error: please configure kubectl for context: ${KUBERCTL_CONTEXT}, got ${CURRENT_CONTEXT}."
-    exit 1
-fi
-
-# TRIGGER DEPLOY
-kubectl rollout restart deployment/${APP_NAME1}
-kubectl rollout restart deployment/${APP_NAME2}
--- build_tarball_deploy_ec2.sh
+++ build_tarball_deploy_ec2.sh
@@ -4,52 +4,25 @@
 INCLUDED_FILES="pynapple/*.py pynapple/requirements.txt deploy"
 TARBALL_NAME="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 LATEST_TARBALL_NAME="deploy_latest.tar.gz"
-S3_BUCKET1="s3://astrolabe-sandbox1-pynapple1-deploy"
-S3_BUCKET2="s3://astrolabe-sandbox1-pynapple2-deploy"
-REFRESH_ASG_INSTANCES=false
-ASG_NAME1="sandbox1-pynapple1-2024033120142773680000000c"
-ASG_NAME2="sandbox1-pynapple2-20240403223052785000000003"
 
-# Parse command-line options
-usage() {
-    echo "Usage: $0 [-r] [-h]"
-    echo "  -r,    Conditionally refresh the ASG."
-    echo "  -h,    Display this help message and exit."
-}
-while getopts ":rh" opt; do
-  case ${opt} in
-    r )
-      REFRESH_ASG_INSTANCES=true
-      ;;
-    h )
-      usage
-      exit
-      ;;
-    \? )
-      usage
-      exit 1
-      ;;
-  esac
-done
+# Validate that at least one bucket is provided
+if [[ $# -eq 0 ]]; then
+    echo "Error: At least one S3 bucket must be specified."
+    echo "Usage: $0 <s3_bucket1> [<s3_bucket2> ...]"
+    exit 1
+fi
 
 # Create a tarball with only the specified files and directories
 tar --exclude '._*' -czvf "$TARBALL_NAME" $(echo $INCLUDED_FILES | sed 's/ / /g')
 
-# Upload the tarball to S3
-aws s3 cp "$TARBALL_NAME" "$S3_BUCKET1/$TARBALL_NAME"
-aws s3 cp "$TARBALL_NAME" "$S3_BUCKET2/$TARBALL_NAME"
-aws s3 cp "$TARBALL_NAME" "$S3_BUCKET1/$LATEST_TARBALL_NAME"
-aws s3 cp "$TARBALL_NAME" "$S3_BUCKET2/$LATEST_TARBALL_NAME"
+# Loop through all arguments and upload to each bucket
+for S3_BUCKET in "$@"; do
+    echo "Uploading to $S3_BUCKET..."
+    aws s3 cp "$TARBALL_NAME" "s3://$S3_BUCKET/$TARBALL_NAME"
+    aws s3 cp "$TARBALL_NAME" "s3://$S3_BUCKET/$LATEST_TARBALL_NAME"
+done
 
 # Optional: Remove the tarball after upload
 rm "$TARBALL_NAME"
 
-echo "Backup completed and uploaded to $S3_BUCKET1"
-echo "Backup completed and uploaded to $S3_BUCKET2"
-
-# Conditional redeployment based on the -r flag
-if [ "$REFRESH_ASG_INSTANCES" = true ] ; then
-  echo "Refreshing ASG instances the service..."
-  aws autoscaling start-instance-refresh --auto-scaling-group-name $ASG_NAME1
-  aws autoscaling start-instance-refresh --auto-scaling-group-name $ASG_NAME2
-fi
+echo "Backup completed and uploaded to all specified buckets."
